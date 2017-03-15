0a49e7ccb6706a489216e90a6b5f5de323886124
Peter2 <erhu65@gmail.com>
2017-03-16 01:12:47 +0800

    init commit

---
+++ routes/index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const passport = require('passport');
+const Account = require('../models/account');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.render('index', { user : req.user });
+});
+
+router.get('/register', (req, res) => {
+    res.render('register', { });
+});
+
+router.post('/register', (req, res, next) => {
+    Account.register(new Account({ username : req.body.username }), req.body.password, (err, account) => {
+        if (err) {
+          return res.render('register', { error : err.message });
+        }
+
+        passport.authenticate('local')(req, res, () => {
+            req.session.save((err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+});
+
+
+router.get('/login', (req, res) => {
+    res.render('login', { user : req.user, error : req.flash('error')});
+});
+
+router.post('/login', passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), (req, res, next) => {
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    req.session.save((err) => {
+        if (err) {
+            return next(err);
+        }
+
+        res.redirect('/');
+    });
+});
+
+router.get('/ping', (req, res) => {
+    res.status(200).send("pong!");
+});
+
+
+router.get('/protected_resource', isAuthenticated, (req, res, next) => {
+
+
+    console.log("user: " + req.user);
+    res.status(200).send("protected_resource!");
+
+
+
+});
+
+function isAuthenticated(req, res, next) {
+    // do any checks you want to in here
+    // CHECK THE USER STORED IN SESSION FOR A CUSTOM VARIABLE
+    // you can do this however you want with whatever variables you set up
+    if (req.user) {
+
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = router;
